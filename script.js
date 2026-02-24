af6c40e2915ac655ad54e93361ba263a5f66789c
jebinraj-byte <santhoshjebinraj@gmail.com>
2026-02-25 00:48:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,94 @@
 
 
+// Loader
+
 setTimeout(()=>{
 
-document.getElementById("loader").style.display="none";
+loader.style.display="none"
 
-},2000);
+},2000)
+
+
+
+
+
+// Scroll button
+
+function scrollToAbout(){
+
+document.getElementById("about").scrollIntoView()
+
+}
+
+
+
+
+// Counter
+
+let n=localStorage.getItem("visit")
+
+if(!n){
+
+n=1
+
+}
+
+else{
+
+n++
+
+}
+
+localStorage.setItem("visit",n)
+
+counter.innerHTML="Visitors "+n
+
+
+
+
+// Chat AI basic
+
+function send(){
+
+let m=msg.value
+
+let t=chatText
+
+
+
+if(m.includes("who")){
+
+t.innerHTML="Jebin is cyber security student"
+
+}
+
+else if(m.includes("project")){
+
+t.innerHTML="He builds servers and AI"
+
+}
+
+else{
+
+t.innerHTML="Ask about Jebin"
+
+}
+
+}
+
+
+
+
+// Particles
+
+particlesJS("particles-js",{
+
+particles:{
+
+number:{value:100},
+
+size:{value:3}
+
+}
+
+})
